98f693533ea6880df4c1f675b983e1cec32f654e
Artemiy Schukin <shuckinart@yandex.ru>
2021-12-31 21:03:49 +0300

    Do not cancel animation on speed or type change

--- src/utils/createAnimation.ts
+++ src/utils/createAnimation.ts
@@ -1,14 +1,12 @@
 import { randNum } from './randNum';
 
-const createDefaultAnimation = (element: HTMLDivElement, speed: number) => {
+export const createDefaultKeyframes = () => {
   const x = Math.random() * 100;
   const y = randNum(-5, 5);
   const offset = randNum(-15, 15);
 
-  const delay = randNum(0, 30) * (-1);
-  const duration = randNum(10, 30);
 
-  const keyframes = [
+  return <Keyframe[]>[
     {
       transform: `translate(${x - offset}vw, ${y}vh)`,
       opacity: 1,
@@ -18,24 +16,25 @@ const createDefaultAnimation = (element: HTMLDivElement, speed: number) => {
       opacity: 1,
     }
   ]
+}
 
-  const animation = element.animate(keyframes, {
+export const createDefaultOptions = () => {
+  const delay = randNum(0, 30) * (-1);
+  const duration = randNum(10, 30);
+
+  return <KeyframeAnimationOptions>{
     duration: duration * 1000,
     delay: delay * 1000,
     iterations: Infinity,
-  });
-
-  animation.playbackRate = speed / 10;
-
-  return animation;
+  }
 }
 
-const createAnotherAnimation = (element: HTMLDivElement, speed: number) => {
+export const createAnotherKeyframes = () => {
   const x = Math.random() * 100;
   const y = randNum(0, 95);
   const offset = randNum(-15, 15);
 
-  const keyframes = [
+  return <Keyframe[]>[
     {
       opacity:  0,
       transform: `translate(${x}vw, ${y}vh)`,
@@ -49,22 +48,16 @@ const createAnotherAnimation = (element: HTMLDivElement, speed: number) => {
       transform: `translate(${x + offset}vw, ${y + 5}vh)`,
     }
   ]
+}
 
-  const animation = element.animate(keyframes, {
+export const createAnotherOptions = () => {
+  return <KeyframeAnimationOptions>{
     duration: randNum(2, 4) * 1000,
     delay: randNum(0, 3) * 1000,
     iterations: Infinity,
-  });
-
-  animation.playbackRate = speed / 10;
-
-  return animation;
+  }
 }
 
-type Style = 0 | 1;
-
-export const createAnimation = (element: HTMLDivElement, speed: number, style: Style = 0) => {
-
-  return style === 0 ? createDefaultAnimation(element, speed) : createAnotherAnimation(element, speed);
-
+export const createAnimation = (element: HTMLElement, Keyframes: Keyframe[], options: KeyframeAnimationOptions): Animation => {
+  return element.animate(Keyframes, options);
 }
