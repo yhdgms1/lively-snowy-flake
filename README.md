ade3e4f8a14f705b9fc97262b2c236f69319e9ff
Artemiy Schukin <shuckinart@yandex.ru>
2022-02-09 12:07:45 +0300

    Up deps; Add download link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Snowy Flake
 
+## Download
+
+[Yandex.Disk](https://disk.yandex.ru/d/Qb-z4MIYiI468Q)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module '*.xht' {
+  export default function (): {
+    $dom: HTMLElement;
+  };
+}
